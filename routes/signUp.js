c0175f4322a2e5796dbdb5c6b86139e08b520326
Seongweon Seo <kissshot93@gmail.com>
2019-05-22 12:41:22 +0900

    signUp_modify

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,50 +1,62 @@
- var express = require('express')
- var app = express()
- var router = express.Router()
- 
- // DATABASE SETTING 
+var express = require('express')
+var router = express.Router()
+
+// DATABASE SETTING
 var connection=require('../configurations/dbConnection');
+
+//LOGGER SETTING
+const logger=require('../configurations/logConfiguration');
+
+
 router.get('/',function(req, res, next) {
-
     var query = connection.query('select * from signUp',
-    function(err,rows){
-        if(rows){
-            res.send(rows)
-        }
-        else{
-            console.log("now rows")
+        function (err, rows) {
+            if (err) {
+                res.send('err : ' + err);
+                throw err;
+            }
+            if (rows[0]) {
+                res.send(rows)
+            } else {
+                res.send('no rows in db');
+            }
+        })
 
-        }
-        
+})
+
+    router.post('/', function(req, res){
+        var body = req.body;
+        var signUp = {
+            'UserID' : req.body.UserID,
+            'UserName' : req.body.UserName,
+            'UserEmail' : req.body.UserEmail,
+            'UserPhoneNumber' : req.body.UserPhoneNumber,
+            'UserGender' : req.body.UserGender,
+            'UserBigcity' : req.body.UserBigcity,
+            'UserPassword' : req.body.UserPassword
+        };
+        //execute sql
+        connection.query("INSERT INTO signUp set ?", signUp,
+            function (error, result, fields){
+
+                var resultMsg={};
+
+                if(error){
+                    //에러 발생시
+                    resultMsg["result"]=0;
+                    res.json(resultMsg);
+                    throw error;
+                }
+                else {
+                    //execution success
+                    resultMsg["result"]=1;
+                    resultMsg["id"]=result.insertId;
+                    res.json(resultMsg);
+                    logger.info(JSON.stringify(userThread)+" insertion success");
+                }
+            })
     })
 
-router.post('/', function(req, res){
 
-    var body = req.body;
-    var signUp = {
-        'UserID' : req.body.UserID,
-        'UserName' : req.body.UserName,
-        'UserEmail' : req.body.UserEmail,
-        'UserPhoneNumber' : req.body.UserPhoneNumber,
-        'UserGender' : req.body.UserGender,
-        'UserBigcity' : req.body.UserBigcity,
-        'UserPassword' : req.body.UserPassword,
-};
-        //execute sql
-        connection.query("INSERT INTO user set ?", signUp,
-        function (error, result, fields){
 
-            if(error){
-                //에러 발생시
-                res.send('err : ' + error)
-            }
-            else {
-                //execution success
-                
-                res.send('success create signup');   
-            }
-        })  
-})
-})
-
- module.exports = router;
+module.exports = router;
