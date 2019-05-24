e2c83c2c25afa17b0e0c44ee172e19e277880112
xuti <xutistory@gmail.com>
2019-05-24 15:30:04 +0900

    PROTOCOL_CONNECTION_LOST 에러 핸들링

--- configurations/dbConnection.js
+++ configurations/dbConnection.js
@@ -1,18 +1,29 @@
 const mysql = require('mysql')
 
+var db_config={
+    host     : '13.125.0.94',
+    port     : 3306,
+    user     : 'root',
+    password : 'teaming',
+    database : 'capswdb',
+    dateStrings:true
+}
+
  // DATABASE SETTING
- const connection = mysql.createConnection({
-     host     : '13.125.0.94',
-     port     : 3306,
-     user     : 'root',
-     password : 'teaming',
-     database : 'capswdb',
-     dateStrings:true
- });
+ var connection = mysql.createConnection(db_config);
 
-connection.connect(function(err){
-    if(err){console.error('mysql connection error :'+err);
-    }
-});
-
+function handleDisconnect(){
+    connection = mysql.createConnection(db_config);
+    connection.connect(function(err){
+        if(err){
+            logger.error('mysql connection error :'+err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+    connection.on('error', function(err) {
+        logger.error('db error', err);
+        setTimeout(handleDisconnect, 2000);
+    });
+}
+handleDisconnect();
 module.exports=connection;
